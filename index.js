39fe02da49aa5acf684a623e49670d0240e26c15
Jeroen Engels <jfm.engels@gmail.com>
2015-01-23 15:03:24 +0100

    Overall improvements. Added package.json and sample config file.

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 var fs 			= require("fs");
 var http 		= require("follow-redirects").http;
+var async 		= require("async");
 var charm 		= require("charm")();
 var mkdirp 		= require("mkdirp");
 var cheerio 	= require("cheerio");
 
+var configFile = "./config.json";
 var program = require('commander');
-var line = 0;
 
 program
   .version('0.0.1')
   .option('-s, --series [series]', 'Series')
-  .option('-c, --chapter [chapter]', 'chapter')
+  .option('-c, --chapter [chapter]', 'Chapter (or first chapter) to download', '1')
   .option('-C, --maxChapter [maxChapter]', 'Chapter up to which to download')
+  .option('-O, --outputDirectory [directory]', 'Setting: Directory in which the files will be downloaded.')
+  .option('-S, --save', 'Save settings so that they will automatically be reused on the next call.')
   .parse(process.argv);
 
-function gracefulExit(message) {
+function gracefulEnd(message) {
 	console.log(message);
 	process.exit(0);
 }
 
 if(!program.series) {
-	gracefulExit("Missing series argument.");
+	gracefulEnd("Missing series argument.");
 }
 if(!program.chapter) {
-	gracefulExit("Missing chapter argument.");
+	gracefulEnd("Missing chapter argument.");
+}
+
+var config = {},
+	defaultConfig = {
+		outputDirectory: "."
+	};
+
+try {
+	var configData = fs.readFileSync(configFile);
+	config = JSON.parse(configData);
+}
+catch(e) {}
+
+if(program.outputDirectory) {
+	config.outputDirectory = program.outputDirectory;
+}
+
+// Manually extending config
+for(var i in defaultConfig) {
+	config[i] = config[i] || defaultConfig[i];
+}
+
+// Save config to file
+if(program.save) {
+	fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
 }
 
 // Utility function that downloads a URL and invokes
@@ -42,51 +70,60 @@ function downloadHTML(url, cb) {
     });
 }
 
-function createOption(series, chapter, maxChapter) {
+function createJob(series, chapter, maxChapter) {
 	var self = this;
 	self.series = series;
 	self.chapter = chapter;
+	self.maxChapter = maxChapter || chapter;
+	self.directory = config.outputDirectory;
 	self.outputFile = function() {
-		return self.series + "/" + self.series + " - " + self.chapter + ".zip";
+		return self.directory + "/" + self.series + "/" + self.series + " " + self.chapter + ".zip";
 	};
 	self.htmlFile = function() {
-		return "http://starkana.com/manga/" + self.series.substring(0, 1) + "/" + self.series.replace(" ", "_") + "/chapter/" + self.chapter;
+		var series = self.series.replace(/\s/g, "_")
+								.replace(/\'/g, "");
+		return "http://starkana.com/manga/" + self.series.substring(0, 1) + "/" + series + "/chapter/" + self.chapter;
 	};
-	self.maxChapter = maxChapter || chapter;
 	return this;
 }
 
-function downloadChapter() {
-	downloadHTML(options.htmlFile(), function(error, data) {
+function downloadChapter(job, cb) {
+	if(!cb) {
+		cb = function() {}
+	}
+	downloadHTML(job.htmlFile(), function(error, data) {
 	    if (error) {
-	    	return gracefulExit(error);
+	    	return gracefulEnd(error);
 	    }
 	    var $ = cheerio.load(data);
 	    if ($("body > center:nth-child(1) > h1").text().indexOf("404") > -1) {
-	    	return gracefulExit("Could not find " + options.series + " " + options.chapter);
+	    	return gracefulEnd("Could not find " + job.series + " " + job.chapter);
 	    }
-	    var downloadUrl = $("#inner_page > div:nth-child(2) > a").attr("href");
+	    if ($("#inner_page > div.ccoi > div:nth-child(1)").text().indexOf("isn't out yet") > -1) {
+	    	return gracefulEnd("Chapter " + job.chapter + " of " + job.series + " isn't out yet.");
+	    }
+	    var downloadUrl = $("#freader-container a.odi").attr("href");
 	    if(!downloadUrl) {
-	    	return gracefulExit("No downloadUrl: " + downloadUrl + "\n" + data);
+	    	return gracefulEnd("No downloadUrl: " + downloadUrl + "\n" + data);
 	    }
 
-	    var file = fs.createWriteStream(options.outputFile());
+	    var file = fs.createWriteStream(job.outputFile());
 	    http.get(downloadUrl, function(res) {
 	    	var timeout = null;
 	    	var totalSize = parseInt(res.headers["content-length"], 10);
 	    	var req = res.pipe(file);
-	    	req.on("error", gracefulExit);
+	    	req.on("error", gracefulEnd);
 
 	    	var size = 0;
 
 	    	res.on("end", function() {
-	    		write(options.chapter + ": " + totalSize + " / " + totalSize + " - 100%");
+	    		write(job.chapter + ": " + totalSize + " / " + totalSize + " - 100%");
 	    		clearTimeout(timeout);
 	    		charm.down(1);
 	    		charm.left(100);
-	    		if (options.chapter < options.maxChapter) {
-	    			options.chapter++;
-	    			downloadChapter();
+	    		if (job.chapter < job.maxChapter) {
+	    			job.chapter++;
+	    			downloadChapter(job);
 	    		}
 	    	});
 
@@ -100,7 +137,7 @@ function downloadChapter() {
 	    	}
 
 	    	function updateProgress() {
-	    		write(options.chapter + ": " + size + " / " + totalSize + " - " + Math.floor(size/totalSize*100) + "%");
+	    		write(job.chapter + ": " + size + " / " + totalSize + " - " + Math.floor(size/totalSize*100) + "%");
 	    		timeout = setTimeout(updateProgress, 100);
 	    	}
 	    	updateProgress();
@@ -110,10 +147,10 @@ function downloadChapter() {
 
 charm.pipe(process.stdout);
 
-var options = createOption(program.series, program.chapter, program.maxChapter);
-mkdirp(options.series, function (error) {
+var startJob = createJob(program.series, program.chapter, program.maxChapter);
+mkdirp(startJob.directory + "/" + startJob.series, function (error) {
     if (error) {
-    	return gracefulExit(error);
+    	return gracefulEnd(error);
 	}
-	downloadChapter();
+	downloadChapter(startJob);
 });
