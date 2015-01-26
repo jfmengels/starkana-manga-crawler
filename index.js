f534fd4b6dededffc18a6c05e3f5fbd4dbafd89b
Jeroen Engels <jfm.engels@gmail.com>
2015-01-26 15:04:47 +0100

    Starting work on changing output format.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require("follow-redirects").http;
 var mkdirp = require("mkdirp");
 var cheerio = require("cheerio");
+var AdmZip = require("adm-zip");
 
 var crawler = {};
 
@@ -46,11 +47,19 @@ crawler.downloadChapter = function($, config, job, chapter, cb) {
     var url = crawler.findChapterLink($, chapter),
         outputFile = config.outputDirectory + "/" + job.series + "/" + job.series + " " + chapter + ".zip",
         file = fs.createWriteStream(outputFile);
+    // TODO Don't write to file unless config.outputFormat is "zip"
 
     http.get(url, function(res) {
+    	var alreadyCalled = false;
+    	function callback(error) {
+    		if (!alreadyCalled) {
+    			alreadyCalled = true;
+    			return cb(error, outputFile);
+    		}
+    	}
         res.pipe(file)
-            .on("error", cb)
-            .on("end", cb);
+            .on("error", callback)
+            .on("end", callback);
     });
 }
 
@@ -59,13 +68,26 @@ crawler.runJob = function(config, job, cb) {
         if (error) {
             return cb(error);
         }
+        // Url example: "http://starkana.com/manga/N/Naruto" for Naruto
         crawler.downloadHTML("http://starkana.com/manga/" + job.series.substring(0, 1) + "/" + job.series, function(error, data) {
             if (error) {
                 return cb(error);
             }
             var $ = cheerio.load(data);
             async.eachLimit(jobs.chapters, 5, function(chapter, cb) {
-                crawler.downloadChapter($, config, job, chapter, cb)
+                crawler.downloadChapter($, config, job, chapter, function(error, zipFile) {
+                	if(error) {
+                		return cb(error);
+                	}
+                	if (config.outputFormat === "zip") {
+                		return cb(null, zipFile);
+                	}
+
+		            var outputFile = zipFile.replace(".zip", "");
+		            var zip = new AdmZip(zipFile);
+		            zip.extractAllTo(outputFile, true);
+                	return cb(null, outputFile);
+                });
             }, cb);
         });
     });
