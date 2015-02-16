4a2b345c58293e6e1169fa75e80f9d87df55b3bb
Jeroen Engels <jfm.engels@gmail.com>
2015-02-16 12:21:09 +0100

    Prevent adding new subscribtions, and stop re-ordering subscribtions on save.

--- subscriber.js
+++ subscriber.js
@@ -4,13 +4,21 @@ subscriber.subscribe = function(series, config, url) {
     if (url && series.length > 1) {
         throw Error("Subscription: Can't guess for which series the url is for.");
     }
-
-    var newSubscriptions = series.map(function(s) {
-        return {
-            name: s,
-            url: url
-        };
+    var currentSubscriptions = config.subscriptions.map(function(s) {
+        return s.name;
     });
+    console.log(currentSubscriptions);
+
+    var newSubscriptions = series
+        .filter(function(s) {
+            return currentSubscriptions.indexOf(s) === -1;
+        })
+        .map(function(s) {
+            return {
+                name: s,
+                url: url
+            };
+        });
     config.subscriptions = config.subscriptions.concat(newSubscriptions);
 };
 
